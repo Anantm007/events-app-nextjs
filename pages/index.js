f46e0125ce17c6d451a21be846373381f811b241
Anant <anant.mathur007@gmail.com>
2021-04-16 14:17:27 +0530

    inital commit, pages setup + homepage complete

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import { getFeaturedEvents } from "../dummy-data";
+import EventList from "../components/events/EventList";
+
 const Home = () => {
+  const featuredEvents = getFeaturedEvents();
   return (
     <div>
-      <h1>Home Page</h1>
+      <EventList events={featuredEvents} />
     </div>
   );
 };
